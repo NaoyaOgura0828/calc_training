54e44a138c5ecd67b2ae5bfa96b7c2f573beab5e
SyatyoAdmiral <naoya_ogura@icloud.com>
2021-07-08 20:52:54 +0900

    'スタート'ボタンのスタイル変更

--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -17,9 +17,25 @@ class _HomeScreenState extends State<HomeScreen> {
               Image.asset('assets/images/image_title.png'),
               Text('問題を選択して「スタート」ボタンを押してください'),
               // TODO: プルダウン選択肢
-              ElevatedButton(
-                  onPressed: null,
-                  child: Text('スタート'))
+              Expanded(
+                child: Container(
+                  alignment: Alignment.bottomCenter, // 画面下部中央にレイアウト
+                  padding: EdgeInsets.only(bottom: 16.0), // ボタンの下部に16.0の余白追加
+                  child: ElevatedButton.icon(
+                    /* 'スタート'ボタン */
+                    icon: Icon(Icons.skip_next),
+                    label: Text('スタート'),
+                    onPressed: () => print('ボタン押下'),
+                    style: ElevatedButton.styleFrom(
+                      primary: Colors.brown, // ボタンの色を茶色に設定
+                      onPrimary: Colors.white, // ボタンのTextを白に設定
+                      shape: RoundedRectangleBorder(
+                        borderRadius: BorderRadius.all(Radius.circular(10.0))
+                      )
+                    ),
+                  ),
+                ),
+              ),
             ],
           ),
         ),
